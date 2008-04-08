fc6b12dafe41cad35463911a96ad5f377c82d296
svcarlsen <svc@kb.dk>
2008-04-08 10:28:25 +0000

    Added Derby accepted comments (--) to header. Uncommented "create index schedulename on schedules(name);", as it is already created automatically by Derby. Gave the WARNING 01504: The new index is a duplicate of an existing index: SQL080408000541731.

--- deploy/deploy/scripts/sql/createfullhddb.sql
+++ deploy/deploy/scripts/sql/createfullhddb.sql
@@ -1,25 +1,25 @@
-/* File:        $Id$
- * Revision:    $Revision$
- * Author:      $Author$
- * Date:        $Date$
- *
- * The Netarchive Suite - Software to harvest and preserve websites
- * Copyright 2004-2007 Det Kongelige Bibliotek and Statsbiblioteket, Denmark
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
+--/* File:        $Id$
+-- * Revision:    $Revision$
+-- * Author:      $Author$
+-- * Date:        $Date$
+-- *
+-- * The Netarchive Suite - Software to harvest and preserve websites
+-- * Copyright 2004-2007 Det Kongelige Bibliotek and Statsbiblioteket, Denmark
+-- *
+-- * This library is free software; you can redistribute it and/or
+-- * modify it under the terms of the GNU Lesser General Public
+-- * License as published by the Free Software Foundation; either
+-- * version 2.1 of the License, or (at your option) any later version.
+-- *
+-- * This library is distributed in the hope that it will be useful,
+-- * but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-- * Lesser General Public License for more details.
+-- *
+-- * You should have received a copy of the GNU Lesser General Public
+-- * License along with this library; if not, write to the Free Software
+-- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+-- */
  
 -- This script will create, but not populate, a Derby database for the
 -- web archiving system.  The database will be created in the current
@@ -153,7 +153,7 @@ connect 'jdbc:derby:fullhddb;create=true';
       onhour int, ondayofweek int, ondayofmonth int, edition bigint not null );
 
  -- Automatically created since column is unique.
- create index schedulename on schedules(name);
+ -- create index schedulename on schedules(name);
 
  insert into schemaversions ( tablename, version ) values ( 'schedules', 1);
 
