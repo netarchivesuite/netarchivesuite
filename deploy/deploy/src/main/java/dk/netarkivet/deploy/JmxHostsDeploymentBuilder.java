460facda643509264e3852cfc127d997f85e2735
kaarefc <kfc@statsbiblioteket.dk>
2008-05-20 13:02:20 +0000

    Remove unused information in jmxhost deployment builder. This is also unittested in previous commits. Whoops.

--- deploy/deploy/src/main/java/dk/netarkivet/deploy/JmxHostsDeploymentBuilder.java
+++ deploy/deploy/src/main/java/dk/netarkivet/deploy/JmxHostsDeploymentBuilder.java
@@ -23,45 +23,24 @@
 
 package dk.netarkivet.deploy;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.dom4j.Document;
+import org.dom4j.DocumentFactory;
 
 import dk.netarkivet.common.exceptions.ArgumentNotValid;
-import dk.netarkivet.common.webinterface.HTMLUtils;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Generates the contents for the monitor_settings.xml, containing a list of
  * hostnames, and a list of JMX-ports associated with each hostname.
  * e.g.:
  * TODO perhaps include the JmxUsername ('monitorRole') in outputXML,
- * TODO perhaps include location information the the outputXML
- * <?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-            + "<settings xmlns=\"http://www.netarkivet.dk/schemas/settings\" "
-            + "xmlns:xsi=\"http://www.w3.org/2001/XMLSc hema-instance\">\n
- * <monitor>
-    <jmxMonitorRolePassword>DetErIkkeVoresSkyld</jmxMonitorRolePassword>
-    <numberOfHosts>13</numberOfHosts>
-    <host1>
-         <name>kb-prod-adm-001.kb.dk</name>
-         <jmxport>8100</jmxport>
-         <jmxport>8101</jmxport>
-    </host1> ..
-
-    <host13>
-         <name>kb-prod-adm-001.kb.dk</name>
-         <jmxport>8100</jmxport>
-         <jmxport>8101</jmxport>
-    </host13>
-    </monitor>
+ * <?xml version="1.0" encoding="UTF-8"?>
+ * <settings xmlns="http://www.netarkivet.dk/schemas/monitor_settings">
+ *     <monitor>
+ *         <jmxMonitorRolePassword>DetErIkkeVoresSkyld</jmxMonitorRolePassword>
+ *     </monitor>
+ * </settings>
  */
 public class JmxHostsDeploymentBuilder implements DeploymentBuilder {
-    
-    private static Log log = LogFactory.getLog(JmxHostsDeploymentBuilder.class.getName());
     /** XML header for the monitor_settings.xml file. */
     private static final String MONITOR_SETTTINGS_HEADER =
             "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
@@ -75,13 +54,8 @@ public class JmxHostsDeploymentBuilder implements DeploymentBuilder {
     private final StringBuilder result;
     /** The general JMX password for the deployment. */
     private String jmxPassword;
-
-    /** a map containing a list for each location of hostnames having jmxports. */
-    private final Map<String, Map<String,List<Integer>>> locationServerMap =
-        new HashMap<String, Map<String,List<Integer>>>();
-    
-    /** The location of the current host. */
-    private String currentLocation;
+    private static final String MONITOR_SETTINGS_NAMESPACE
+            = "http://www.netarkivet.dk/schemas/monitor_settings";
 
     /**
      * Register the factory used for establishing connections to remote servers.
@@ -99,120 +73,39 @@ public class JmxHostsDeploymentBuilder implements DeploymentBuilder {
      */
     public void setJmxPassword(String password) {
         ArgumentNotValid.checkNotNullOrEmpty(password, "String password");
-        this.jmxPassword = password;
+        jmxPassword = password;
     }
 
     /**
-     * Returns an object that registers information about the JMXport
-     * and RMIPort for each remote application
-     * on the given host.
-     * @return an HostBuilder object that registers information
-     * about the JMXport and RMIPort
-     * for each remote application on the given host.
+     * Currently the host information is not used by this builder, so just
+     * return a hostbilder that does nothing.
+     * @return an HostBuilder object that ignores information about a host.
      */
     public HostBuilder newHostBuilder() {
         return new HostBuilder() {
-            private String server;
-            public void setName(String serverName) {
-                server = serverName;
-                Map<String, List<Integer>> serversOnLocation;
-                if (locationServerMap.containsKey(currentLocation)) {
-                    serversOnLocation = locationServerMap.get(currentLocation);
-                } else {
-                    serversOnLocation = new HashMap<String, List<Integer>>();
-                }                     
-                locationServerMap.put(currentLocation, serversOnLocation);
-           }
-            
-            public void addJmxPort(int jmxPortNo) {
-                ArgumentNotValid.checkPositive(jmxPortNo, "int jmxPortNo");
-                List<Integer> entriesForServer;
-                Map<String, List<Integer>> serversOnLocation =
-                    locationServerMap.get(currentLocation);
-                if (!serversOnLocation.containsKey(server)) {
-                    entriesForServer = serversOnLocation.put(server,
-                            new ArrayList<Integer>());
-                }
-                entriesForServer = serversOnLocation.get(server);
-                entriesForServer.add(new Integer(jmxPortNo));
-                serversOnLocation.put(server, entriesForServer);
-                
-                // Update the locationServerMap
-                locationServerMap.put(currentLocation, serversOnLocation);
-            }
-
-            /**
-             * Nothing needs to be done here! The action is postponed to
-             * JmxProxyDeploymentBuilder.done().
-             */
-            public void done() {
-            }
+            public void setName(String serverName) {}
+            public void addJmxPort(int jmxPortNo) {}
+            public void done() {}
         };
     }
 
     /**
      * Remembers the current locations, but currently does not use it.
-     * @see dk.netarkivet.deploy.DeploymentBuilder#setLocation(java.lang.String)
+     * @see DeploymentBuilder#setLocation(String)
      */
     public void setLocation(String location) {
-        ArgumentNotValid.checkNotNullOrEmpty(location, "String location");
-        this.currentLocation = location;
     }
 
     /**
      * Done collection information, generate resulting XML.
-     * @see dk.netarkivet.deploy.DeploymentBuilder#done()
+     * @see DeploymentBuilder#done()
      */
     public void done() {
-        // We have now finished collecting the information.
-        // Now the real work begins.
-        // Check, that the JMX password is set. We need it now.
-        ArgumentNotValid.checkNotNullOrEmpty(jmxPassword,
-                "String jmxPassword");
-        result.append(MONITOR_SETTTINGS_HEADER);
-        result.append("<monitor>\n");
-        result.append("<jmxMonitorRolePassword>");
-        result.append(HTMLUtils.escapeHtmlValues(jmxPassword));
-        result.append("</jmxMonitorRolePassword>\n");
-        result.append("<numberOfHosts>");
-        int hosts = 0;
-        for (String locationName: locationServerMap.keySet()){
-            hosts = hosts + locationServerMap.get(locationName).keySet().size();
-        }
-        result.append(hosts);
-        result.append("</numberOfHosts>\n");
-
-        // No need for now to distinguish between hosts from different locations,
-        // so location information is presently discarded in this builder.
-        
-        int i = 0;
-        for (String locationName: locationServerMap.keySet()) {
-            log.trace("Writing the servers on location '"
-                    + locationName + "'.");
-            Map<String, List<Integer>> serversOnLocation
-                    = locationServerMap.get(locationName);
-            
-            log.trace("Writing " + serversOnLocation.keySet().size() 
-                    + " servers on location '" + locationName + "'.");
-            for (String hostName: serversOnLocation.keySet()) {
-                List<Integer> jmxPorts = serversOnLocation.get(hostName);
-                if (jmxPorts.size() > 0){
-                    i++;
-                    result.append("<host" + i + ">\n");
-                    result.append("<name>");
-                    result.append(HTMLUtils.escapeHtmlValues(hostName));
-                    result.append("</name>\n");
-
-                    for (Integer jmxPort : jmxPorts) {
-                        result.append("<jmxport>");
-                        result.append(jmxPort.longValue());
-                        result.append("</jmxport>\n");
-                    }
-                    result.append("</host" + i + ">\n");
-                }
-            }
-        }
-        result.append("</monitor>\n");
-        result.append(MONITOR_SETTTINGS_FOOTER);
+        Document d = DocumentFactory.getInstance().createDocument();
+        d.addElement("settings", MONITOR_SETTINGS_NAMESPACE).addElement(
+                "monitor", MONITOR_SETTINGS_NAMESPACE).addElement(
+                "jmxMonitorRolePassword", MONITOR_SETTINGS_NAMESPACE).setText(
+                jmxPassword);
+        result.append(d.asXML());
     }
 }
