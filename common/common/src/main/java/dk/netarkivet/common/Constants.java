e8b9be5a1f8952fc08fde5148a9c55072102114f
svcarlsen <svc@kb.dk>
2008-07-04 15:09:30 +0000

    Set branch 3.6.1 to BUILDSTATUS.RELEASE

--- common/common/src/main/java/dk/netarkivet/common/Constants.java
+++ common/common/src/main/java/dk/netarkivet/common/Constants.java
@@ -78,7 +78,7 @@ public class Constants {
     /** Patch version number. */
     public static final int PATCHVERSION = 1;
     /** Current status of code. */
-    private static final CodeStatus BUILDSTATUS = CodeStatus.CODEFREEZE;
+    private static final CodeStatus BUILDSTATUS = CodeStatus.RELEASE;
 
     /** Current version of Heritrix used by netarkivet-code. */
     private static final String HERITRIX_VERSION = "1.12.1b";
