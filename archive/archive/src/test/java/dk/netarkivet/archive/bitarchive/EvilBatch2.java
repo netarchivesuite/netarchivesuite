94328a8f1d44a7d2b527a63ee5dee5fa8d1e8bb6
elzikb <elzi@kb.dk>
2008-09-26 13:44:27 +0000

    For test of preventing deltion of files in bit archive

---
+++ archive/archive/src/test/java/dk/netarkivet/archive/bitarchive/EvilBatch2.java
@@ -0,0 +1,30 @@
+package dk.netarkivet.archive.bitarchive;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import dk.netarkivet.common.exceptions.IOFailure;
+import dk.netarkivet.common.utils.FileUtils;
+import dk.netarkivet.common.utils.arc.FileBatchJob;
+
+/** This class attempts to do illegal actions.
+ */
+public class EvilBatch2 extends FileBatchJob {
+    public void initialize(OutputStream os) {
+    }
+    
+    public boolean processFile(File file, OutputStream os) {
+        try {
+            file.delete();
+        } catch (SecurityException e) {
+            return false;
+        }
+        return true;
+    }
+    
+    public void finish(OutputStream os) {
+    }
+}
