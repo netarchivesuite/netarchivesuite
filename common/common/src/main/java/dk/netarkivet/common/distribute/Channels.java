682901cb83fa4a40c5a62c71229ac24088980780
elzikb <elzi@kb.dk>
2009-01-15 10:23:18 +0000

    Correction of channels in connection with:
    location renamed to replica and use is renamed to replica use or physical location use
    applicationInstanceId as new setting
    This is part of implementation of assignment deploy 1: Assignment - Rewrite deploy based on settings overwrite
    and 
    Assignment B.2.2a0 - Separate the meaning of replica and physical location

--- common/common/src/main/java/dk/netarkivet/common/distribute/Channels.java
+++ common/common/src/main/java/dk/netarkivet/common/distribute/Channels.java
@@ -63,34 +63,53 @@ public class Channels {
 
     /**
      * thisReplica is the replica, used for applications
-     * that only communicate with local processes. An example value is "KB".
+     * that represent a replica. An example value is "KB".
      */
     private final String thisReplica = Settings.get(
             CommonSettings.ENVIRONMENT_THIS_REPLICA_ID);
+
+    /**
+     * useReplica is the replica, used for applications
+     * that only communicate with local processes. An example value is "KB".
+     */
+    private final String useReplica = Settings.get(
+            CommonSettings.ENVIRONMENT_USE_REPLICA_ID);
 
     /** The index of this replica in the allReplicas list. */
     private final int indexOfThisReplica = Arrays.asList(allReplicas)
             .indexOf(thisReplica);
 
+    /** The index of use replica in the allReplicas list. */
+    private final int indexOfUseReplica = Arrays.asList(allReplicas)
+            .indexOf(thisReplica);
+    
     private Channels() {
-        if (indexOfThisReplica < 0) {
-            throw new ArgumentNotValid("Bad replica '" + thisReplica + "'");
+        int index = indexOfThisReplica;
+        if (index < 0) {
+            if (indexOfUseReplica < 0) {
+                throw new ArgumentNotValid("Bad replicas " 
+                        + "thisReplica: '" + thisReplica + "'"
+                        + "useReplica: '" + useReplica + "'");
+            } else {
+                index = indexOfUseReplica;
+            }
         }
+
         for (int i = 0; i < allReplicas.length; i++) {
             ALL_BA_ARRAY[i] = new ChannelID("ALL_BA", allReplicas[i],
                     ChannelID.NO_IP, ChannelID.NO_PROC_ID, ChannelID.TOPIC);
         }
-        ALL_BA = ALL_BA_ARRAY[indexOfThisReplica];
+        ALL_BA = ALL_BA_ARRAY[index];
         for (int i = 0; i < allReplicas.length; i++) {
             ANY_BA_ARRAY[i] = new ChannelID("ANY_BA", allReplicas[i],
                     ChannelID.NO_IP, ChannelID.NO_PROC_ID, ChannelID.QUEUE);
         }
-        ANY_BA = ANY_BA_ARRAY[indexOfThisReplica];
+        ANY_BA = ANY_BA_ARRAY[index];
         for (int i = 0; i < allReplicas.length; i++) {
             THE_BAMON_ARRAY[i] = new ChannelID("THE_BAMON", allReplicas[i],
                     ChannelID.NO_IP, ChannelID.NO_PROC_ID, ChannelID.QUEUE);
         }
-        THE_BAMON = THE_BAMON_ARRAY[indexOfThisReplica];
+        THE_BAMON = THE_BAMON_ARRAY[index];
     }
 
     /* ******** Sort in order of the Distributed Architecture paper ******** */
