1920615028cc71ca483a16174c62a89382cae777
jolf <jolf@kb.dk>
2009-02-12 14:43:19 +0000

    Gives the replica name instead of the replica id.

--- common/common/src/main/java/dk/netarkivet/common/management/SingleMBeanObject.java
+++ common/common/src/main/java/dk/netarkivet/common/management/SingleMBeanObject.java
@@ -34,6 +34,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import dk.netarkivet.common.CommonSettings;
+import dk.netarkivet.common.distribute.arcrepository.Replica;
 import dk.netarkivet.common.exceptions.ArgumentNotValid;
 import dk.netarkivet.common.exceptions.IOFailure;
 import dk.netarkivet.common.exceptions.IllegalState;
@@ -131,7 +132,8 @@ public class SingleMBeanObject<I> {
             nameProperties.put(Constants.PRIORITY_KEY_PRIORITY, "");            
         }
         try {
-            String val = Settings.get(CommonSettings.USE_REPLICA_ID);
+            String val = Replica.getReplicaFromId(Settings.get(
+        	    CommonSettings.USE_REPLICA_ID)).getName();
             nameProperties.put(Constants.PRIORITY_KEY_REPLICA,  val);
         } catch (Exception e) {
             nameProperties.put(Constants.PRIORITY_KEY_REPLICA,  "");
