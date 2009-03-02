deea6d63654e09f789dfb6b3a51e749db4354451
jolf <jolf@kb.dk>
2009-03-02 13:13:56 +0000

    Removed old unused test files

--- deploy/deploy/src/test/resources/dk/netarkivet/deploy/data/originals/single_target/kb-test-bar-012.bitarkiv.kb.dk/startall.bat
+++
@@ -1,3 +0,0 @@
-echo Starting all applications on: 'kb-test-bar-012.bitarkiv.kb.dk'
-cd "c:\Documents and Settings\dev\TEST\conf\"
-"C:\Program Files\Bitvise WinSSHD\bvRun" -brj -new -cmd="start_BitarchiveApplication.bat"
--- deploy/deploy/src/test/resources/dk/netarkivet/deploy/data/originals/single_target/kb-test-bar-013.bitarkiv.kb.dk/killall.bat
+++
@@ -1,3 +0,0 @@
-echo Killing all applications on: 'kb-test-bar-013.bitarkiv.kb.dk'
-cd "c:\Documents and Settings\dev\TEST\conf\"
-"C:\Program Files\Bitvise WinSSHD\bvRun" -brj -new -cmd="kill_BitarchiveApplication.bat"
