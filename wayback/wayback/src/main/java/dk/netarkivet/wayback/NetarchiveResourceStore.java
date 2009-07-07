82bd2b33c8e88faf65c57f1bd510229bdb0b2e86
svcarlsen <svc@kb.dk>
2009-07-07 13:43:17 +0000

    Added missing svn-keywords to some java-files, and corrected one header

--- wayback/wayback/src/main/java/dk/netarkivet/wayback/NetarchiveResourceStore.java
+++ wayback/wayback/src/main/java/dk/netarkivet/wayback/NetarchiveResourceStore.java
@@ -1,3 +1,25 @@
+/* $Id$
+ * $Revision$
+ * $Date$
+ * $Author$
+ *
+ * The Netarchive Suite - Software to harvest and preserve websites
+ * Copyright 2004-2007 Det Kongelige Bibliotek and Statsbiblioteket, Denmark
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package dk.netarkivet.wayback;
 
 import java.io.IOException;
@@ -22,29 +44,6 @@ import dk.netarkivet.archive.arcrepository.distribute.JMSArcRepositoryClient;
 import dk.netarkivet.common.distribute.arcrepository.ArcRepositoryClient;
 import dk.netarkivet.common.distribute.arcrepository.BitarchiveRecord;
 import dk.netarkivet.common.utils.InputStreamUtils;
-
-/* $ ID: NetarchiveResourceStore.java Jun 15, 2009 10:28:22 AM hbk $
-* $ Revision: $
-* $ Date: Jun 15, 2009 10:28:22 AM $ 
-* $ @auther hbk $
-* The Netarchive Suite - Software to harvest and preserve websites
-* Copyright 2004-2007 Det Kongelige Bibliotek and Statsbiblioteket, Denmark
-*
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2.1 of the License, or (at your option) any later version.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this library; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
 
 public class NetarchiveResourceStore implements ResourceStore {
 
