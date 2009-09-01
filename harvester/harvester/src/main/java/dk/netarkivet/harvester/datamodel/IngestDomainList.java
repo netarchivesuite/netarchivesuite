f9ea527aa2f4c4a136f61acf7cf8bb5b944a11ff
svcarlsen <svc@kb.dk>
2009-09-01 12:59:53 +0000

    Tabs removed from file; added javadoc for the constructor.

--- harvester/harvester/src/main/java/dk/netarkivet/harvester/datamodel/IngestDomainList.java
+++ harvester/harvester/src/main/java/dk/netarkivet/harvester/datamodel/IngestDomainList.java
@@ -45,85 +45,95 @@ import dk.netarkivet.harvester.Constants;
  * Utility class for ingesting new domains into the database.
  */
 public class IngestDomainList {
-	/** I18n bundle used by this class. */
-	private static final I18n I18N = new dk.netarkivet.common.utils.I18n(
-	        Constants.TRANSLATIONS_BUNDLE);
-	/** How often to log progress. */
-	private static final int PRINT_INTERVAL = 10000;
-	
+    /** I18n bundle used by this class. */
+    private static final I18n I18N = new dk.netarkivet.common.utils.I18n(
+            Constants.TRANSLATIONS_BUNDLE);
+
+    /** How often to log progress. */
+    private static final int PRINT_INTERVAL = 10000;
+    
+    /** The logger. */
     protected final Log log = LogFactory.getLog(getClass());
+
     /** Connection to the persistent store of Domains. */
     private DomainDAO dao;
-
+    
+    /**
+     * Constructor for the IngestDomainList class.
+     * It makes a connection to the Domains store.
+     */
     public IngestDomainList() {
         dao = DomainDAO.getInstance();
     }
 
     /**
-     * Adds all new domains from a newline-separated file of domain names.
-     * The file is assumed to be in the UTF-8 format.
-     * For large files, a line is printed to the log, and to the out variable 
-     * (if not set to null), every PRINT_INTERVAL lines. 
-     * @param domainList the file containing the domain names.
-     * @param out a stream to which output can be sent. May be null.
-     * @param theLocale the given Locale
+     * Adds all new domains from a newline-separated file of domain names. The
+     * file is assumed to be in the UTF-8 format. For large files, a line is
+     * printed to the log, and to the out variable (if not set to null), every
+     * PRINT_INTERVAL lines.
+     * 
+     * @param domainList
+     *            the file containing the domain names.
+     * @param out
+     *            a stream to which output can be sent. May be null.
+     * @param theLocale
+     *            the given Locale
      */
     public void updateDomainInfo(File domainList, JspWriter out,
-    		Locale theLocale) {
-    	ArgumentNotValid.checkNotNull(domainList, "File domainList");
-    	ArgumentNotValid.checkNotNull(theLocale, "Locale theLocale");
+            Locale theLocale) {
+        ArgumentNotValid.checkNotNull(domainList, "File domainList");
+        ArgumentNotValid.checkNotNull(theLocale, "Locale theLocale");
         Domain myDomain;
         String domainName;
         BufferedReader in = null;
         int countDomains = 0;
         boolean print = (out != null);
         try {
-            in = new BufferedReader(new InputStreamReader(
-                    new FileInputStream(domainList), "UTF-8"));
+            in = new BufferedReader(new InputStreamReader(new FileInputStream(
+                    domainList), "UTF-8"));
 
             while ((domainName = in.readLine()) != null) {
                 try {
-                	countDomains++;
+                    countDomains++;
                     if ((countDomains % PRINT_INTERVAL) == 0) {
-                    	Date d = new Date();
-                    	String msg = "Domain #" + countDomains + ": "
-                            + domainName + " added at " + d;
+                        Date d = new Date();
+                        String msg = "Domain #" + countDomains + ": "
+                                + domainName + " added at " + d;
                         log.info(msg);
                         if (print) {
-                            out.print(I18N.getString(theLocale, 
-                            		"domain.number.0.1.added.at.2", 
-                            		countDomains, domainName, d));
+                            out.print(I18N.getString(theLocale,
+                                    "domain.number.0.1.added.at.2",
+                                    countDomains, domainName, d));
                             out.print("<br/>");
                             out.flush();
                         }
                     }
-                    
+
                     if (DomainUtils.isValidDomainName(domainName)) {
                         if (!dao.exists(domainName)) {
                             myDomain = Domain.getDefaultDomain(domainName);
                             dao.create(myDomain);
                         }
                     } else {
-                        log.debug("domain '"
-                                    + domainName +
-                                    "' is not a valid domain Name");
+                        log.debug("domain '" + domainName
+                                + "' is not a valid domain Name");
                         if (print) {
-                        	out.print(I18N.getString(theLocale,                  			
-                        			"errormsg;domain.0.is.not.a.valid"
-                        			+ ".domainname",
-                        			domainName));
+                            out.print(I18N.getString(theLocale,
+                                    "errormsg;domain.0.is.not.a.valid"
+                                            + ".domainname", domainName));
                             out.print("<br/>");
                             out.flush();
                         }
                     }
                 } catch (Exception e) {
-                    log.debug("Could not create domain '"
-                              + domainName + "'", e);
-                    if (print) {               	
-                    	out.print(I18N.getString(theLocale,                  			
-                    			"errormsg;unable.to.create"
-                    			+ ".domain.0.due.to.error.1",
-                    			domainName, e.getMessage()));
+                    log
+                            .debug("Could not create domain '" + domainName
+                                    + "'", e);
+                    if (print) {
+                        out.print(I18N.getString(theLocale,
+                                "errormsg;unable.to.create"
+                                        + ".domain.0.due.to.error.1",
+                                domainName, e.getMessage()));
                         out.print("<br/>\n");
                         out.flush();
                     }
@@ -131,12 +141,12 @@ public class IngestDomainList {
             }
         } catch (FileNotFoundException fnf) {
             String msg = "File '" + domainList.getAbsolutePath()
-            	+ "' not found";
+                    + "' not found";
             log.debug(msg);
             throw new IOFailure(msg, fnf);
         } catch (IOException io) {
-            String msg = " Can't read the domain-file '" 
-            	+ domainList.getAbsolutePath() + "'.";
+            String msg = " Can't read the domain-file '"
+                    + domainList.getAbsolutePath() + "'.";
             log.debug(msg);
             throw new IOFailure(msg, io);
         } finally {
