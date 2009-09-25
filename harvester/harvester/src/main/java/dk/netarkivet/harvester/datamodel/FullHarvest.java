21e04f7acea224e0f967be5fa2f89bd31a242214
kaarefc <kfc@statsbiblioteket.dk>
2009-09-25 11:40:19 +0000

    An extra test for object limit added after FR 1689 implementation

--- harvester/harvester/src/main/java/dk/netarkivet/harvester/datamodel/FullHarvest.java
+++ harvester/harvester/src/main/java/dk/netarkivet/harvester/datamodel/FullHarvest.java
@@ -34,41 +34,39 @@ import dk.netarkivet.common.exceptions.UnknownID;
 import dk.netarkivet.common.utils.FilterIterator;
 
 /**
- * This class contains the specific
- * properties and operations of snapshot harvest definitions.
- *
+ * This class contains the specific properties and operations of snapshot
+ * harvest definitions.
  */
 public class FullHarvest extends HarvestDefinition {
     private final Log log = LogFactory.getLog(getClass());
 
     /**
-     * The maximum number of objects retrieved from each domain
-     * during a snapshot harvest.
+     * The maximum number of objects retrieved from each domain during a
+     * snapshot harvest.
      */
     private long maxCountObjects;
 
     /**
-     * The maximum number of bytes retrieved from each domain during a
-     * snapshot harvest.
+     * The maximum number of bytes retrieved from each domain during a snapshot
+     * harvest.
      */
     private long maxBytes;
 
-    /** The ID for the harvestdefinition, this FullHarvest is
-     *  based upon.
-     */
+    /** The ID for the harvestdefinition, this FullHarvest is based upon. */
     private Long previousHarvestDefinitionOid;
 
     /**
-     * Create new instance of FullHarvest configured according
-     * to the properties of the supplied DomainConfiguration.
+     * Create new instance of FullHarvest configured according to the properties
+     * of the supplied DomainConfiguration.
      *
-     * @param harvestDefName the name of the harvest definition
-     * @param comments       comments
+     * @param harvestDefName               the name of the harvest definition
+     * @param comments                     comments
      * @param previousHarvestDefinitionOid This harvestDefinition is used to
-     * create this Fullharvest definition.
-     * @param maxCountObjects Limit for how many objects can be fetched per
-     * domain
-     * @param maxBytes Limit for how many bytes can be fetched per domain
+     *                                     create this Fullharvest definition.
+     * @param maxCountObjects              Limit for how many objects can be
+     *                                     fetched per domain
+     * @param maxBytes                     Limit for how many bytes can be
+     *                                     fetched per domain
      */
     public FullHarvest(String harvestDefName,
                        String comments,
@@ -89,11 +87,12 @@ public class FullHarvest extends HarvestDefinition {
      * Get a new Job suited for this type of HarvestDefinition.
      *
      * @param cfg The configuration to use when creating the job
+     *
      * @return a new job
      */
     protected Job getNewJob(DomainConfiguration cfg) {
         return Job.createSnapShotJob(getOid(), cfg, this.getMaxCountObjects(),
-                this.getMaxBytes(), numEvents);
+                                     this.getMaxBytes(), numEvents);
     }
 
     /**
@@ -109,36 +108,37 @@ public class FullHarvest extends HarvestDefinition {
         return null;
     }
 
-    /** Set the previous HarvestDefinition which is used to base this.
-     *  @param prev The id of a HarvestDefinition
+    /**
+     * Set the previous HarvestDefinition which is used to base this.
+     *
+     * @param prev The id of a HarvestDefinition
      */
     public void setPreviousHarvestDefinition(Long prev) {
         previousHarvestDefinitionOid = prev;
     }
 
-    /**
-     * @return Returns the maxCountObjects.
-     */
+    /** @return Returns the maxCountObjects. */
     public long getMaxCountObjects() {
         return maxCountObjects;
     }
 
-    /**
-     * @param maxCountObjects The maxCountObjects to set.
-     */
+    /** @param maxCountObjects The maxCountObjects to set. */
     public void setMaxCountObjects(long maxCountObjects) {
         this.maxCountObjects = maxCountObjects;
     }
 
-    /** Get the maximum number of bytes that this fullharvest will harvest
-     * per domain, 0 for no limit.
+    /**
+     * Get the maximum number of bytes that this fullharvest will harvest per
+     * domain, 0 for no limit.
+     *
      * @return Total download limit in bytes per domain.
      */
     public long getMaxBytes() {
         return maxBytes;
     }
 
-    /** Set the limit for how many bytes this fullharvest will harvest per
+    /**
+     * Set the limit for how many bytes this fullharvest will harvest per
      * domain, or -1 for no limit.
      *
      * @param maxBytes Number of bytes to stop harvesting at.
@@ -148,7 +148,8 @@ public class FullHarvest extends HarvestDefinition {
     }
 
     /**
-     * Returns an iterator of domain configurations for this harvest definition.
+     * Returns an iterator of domain configurations for this harvest
+     * definition.
      *
      * @return Iterator containing information about the domain configurations
      */
@@ -163,33 +164,49 @@ public class FullHarvest extends HarvestDefinition {
         final DomainDAO dao = DomainDAO.getInstance();
         //Get what has been harvested
         Iterator<HarvestInfo> i =
-            dao.getHarvestInfoBasedOnPreviousHarvestDefinition(
-                getPreviousHarvestDefinition());
+                dao.getHarvestInfoBasedOnPreviousHarvestDefinition(
+                        getPreviousHarvestDefinition());
         // Filter out HarvestInfo objects for domains that either
         // 1) are completed
         // 2) have reached their maxBytes limit
         //   (and the maxBytes limit has not changed since time of harvest)
+        // 2) have reached their maxObjects limit
+        //   (and the maxObjects limit has not changed since time of harvest)
         // 3) that are current aliases of another domain
         //
         // and get domain configurations for the rest.
         return new FilterIterator<HarvestInfo, DomainConfiguration>(i) {
             protected DomainConfiguration filter(HarvestInfo harvestInfo) {
                 if (harvestInfo.getStopReason()
-                     == StopReason.DOWNLOAD_COMPLETE) {
+                    == StopReason.DOWNLOAD_COMPLETE) {
                     // Don't include the ones that finished.
                     return null;
                 }
 
                 DomainConfiguration config
-                = getConfigurationFromPreviousHarvest(harvestInfo, dao);
+                        = getConfigurationFromPreviousHarvest(harvestInfo, dao);
                 if (harvestInfo.getStopReason()
-                        == StopReason.CONFIG_SIZE_LIMIT) {
+                    == StopReason.CONFIG_SIZE_LIMIT) {
                     // Check if MaxBytes limit for DomainConfiguration have
                     // been raised since previous harvest.
                     // If this is the case, return the configuration
                     int compare = NumberUtils.compareInf(config.getMaxBytes(),
-                            harvestInfo.getSizeDataRetrieved());
-                    if (compare < 1){
+                                                         harvestInfo.getSizeDataRetrieved());
+                    if (compare < 1) {
+                        return null;
+                    } else {
+                        return config;
+                    }
+                }
+
+                if (harvestInfo.getStopReason()
+                    == StopReason.CONFIG_OBJECT_LIMIT) {
+                    // Check if MaxObjects limit for DomainConfiguration have
+                    // been raised since previous harvest.
+                    // If this is the case, return the configuration
+                    int compare = NumberUtils.compareInf(config.getMaxObjects(),
+                                                         harvestInfo.getCountObjectRetrieved());
+                    if (compare < 1) {
                         return null;
                     } else {
                         return config;
@@ -208,14 +225,16 @@ public class FullHarvest extends HarvestDefinition {
     }
 
 
-    /** Get the configuration used in a previous harvest.  If the
-     * configuration in the harvestinfo cannot be found (deleted), uses
-     * the default configuration.
+    /**
+     * Get the configuration used in a previous harvest.  If the configuration
+     * in the harvestinfo cannot be found (deleted), uses the default
+     * configuration.
      *
      * @param harvestInfo A harvest info object from a previous harvest.
-     * @param dao The dao to read configurations from.
-     * @return A configuration if found and the download in this harvestinfo
-     * was complete, null otherwise
+     * @param dao         The dao to read configurations from.
+     *
+     * @return A configuration if found and the download in this harvestinfo was
+     *         complete, null otherwise
      */
     private DomainConfiguration getConfigurationFromPreviousHarvest(
             final HarvestInfo harvestInfo, DomainDAO dao) {
@@ -233,12 +252,12 @@ public class FullHarvest extends HarvestDefinition {
                 //back on default configuration
                 configuration = domain.getDefaultConfiguration();
                 log.debug("Previous configuration '"
-                        + harvestInfo.getDomainConfigurationName()
-                        + "' for harvesting domain '"
-                        + harvestInfo.getDomainName()
-                        + "' not found. Using default '"
-                        + configuration.getName()
-                        + "' instead.", e);
+                          + harvestInfo.getDomainConfigurationName()
+                          + "' for harvesting domain '"
+                          + harvestInfo.getDomainName()
+                          + "' not found. Using default '"
+                          + configuration.getName()
+                          + "' instead.", e);
             }
             //Add the configuration to the list to harvest
             return configuration;
@@ -262,6 +281,7 @@ public class FullHarvest extends HarvestDefinition {
      * Check if this harvest definition should be run, given the time now.
      *
      * @param now The current time
+     *
      * @return true if harvest definition should be run
      */
     public boolean runNow(Date now) {
