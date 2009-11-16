b3b211f8962b98dd057b964070f4c212c0f11501
kaarefc <kfc@statsbiblioteket.dk>
2009-11-16 10:57:02 +0000

    Release of stable version 3.10.0

--- common/common/src/main/java/dk/netarkivet/common/Constants.java
+++ common/common/src/main/java/dk/netarkivet/common/Constants.java
@@ -74,9 +74,9 @@ public class Constants {
     /** Major version number. */
     public static final int MAJORVERSION = 3;
     /** Minor version number. */
-    public static final int MINORVERSION = 9;
+    public static final int MINORVERSION = 10;
     /** Patch version number. */
-    public static final int PATCHVERSION = 2;
+    public static final int PATCHVERSION = 0;
     /** Current status of code. */
     private static final CodeStatus BUILDSTATUS = CodeStatus.CODEFREEZE;
 
