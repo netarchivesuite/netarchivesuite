47f1aa814d2eb144880b89daa4becc10b080fdab
svcarlsen <svc@kb.dk>
2010-02-08 10:58:18 +0000

    Moved ArcFileRemover to correct package dk.netarkivet.common.utils.batch, renamed it as FileRemover as it can handle any file and not just arcfiles.

--- common/common/src/main/java/dk/netarkivet/common/utils/batch/FileRemover.java
+++ common/common/src/main/java/dk/netarkivet/common/utils/batch/FileRemover.java
@@ -1,7 +1,7 @@
-/* File:       $Id: Bitarchive.java 752 2009-03-05 18:09:21Z svc $
- * Revision:   $Revision: 752 $
- * Author:     $Author: svc $
- * Date:       $Date: 2009-03-05 19:09:21 +0100 (to, 05 mar 2009) $
+/* File:       $Id$
+ * Revision:   $Revision$
+ * Author:     $Author$
+ * Date:       $Date$
  *
  * The Netarchive Suite - Software to harvest and preserve websites
  * Copyright 2004-2007 Det Kongelige Bibliotek and Statsbiblioteket, Denmark
@@ -21,18 +21,17 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-package dk.netarkivet.archive.arcrepository.distribute;
+package dk.netarkivet.common.utils.batch;
 
 import java.io.File;
 import java.io.OutputStream;
 
-import dk.netarkivet.common.utils.batch.FileBatchJob;
 
 /**
  * @author ngiraud
  *
  */
-public class ArcFileRemover extends FileBatchJob {
+public class FileRemover extends FileBatchJob {
 
 	@Override
 	public void finish(OutputStream os) {
