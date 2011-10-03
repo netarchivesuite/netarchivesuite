4442289c627c48b0604bfb54115edd70cab10dfe
mikisseth <mss@statsbiblioteket.dk>
2011-10-03 11:22:46 +0000

    NAS-6: Create Maven Proof-of-concept build Fixed symbolic links creation script.

--- unclassified/create-maven-links.sh
+++ unclassified/create-maven-links.sh
@@ -13,36 +13,45 @@ cd $PROJECT_HOME
 M2_BUILD_HOME=./m2-build
 
 # Common module
-rm $M2_BUILD_HOME/netarchivesuite-common/src/test/java/dk/netarkivet/testutils
-ln -s $PWD/tests/dk/netarkivet/testutils $M2_BUILD_HOME/netarchivesuite-common/src/test/java/dk/netarkivet/
-
 rm $M2_BUILD_HOME/netarchivesuite-common/src/main/java/dk/netarkivet/common
-ln -s $PWD/src/dk/netarkivet/common $M2_BUILD_HOME/netarchivesuite-common/src/main/java/dk/netarkivet/
+ln -s $PWD/src/dk/netarkivet/common $M2_BUILD_HOME/netarchivesuite-common/src/main/java/dk/netarkivet
 rm $M2_BUILD_HOME/netarchivesuite-common/src/test/java/dk/netarkivet/common
-ln -s $PWD/tests/dk/netarkivet/common $M2_BUILD_HOME/netarchivesuite-common/src/test/java/dk/netarkivet/
+ln -s $PWD/tests/dk/netarkivet/common $M2_BUILD_HOME/netarchivesuite-common/src/test/java/dk/netarkivet
 
 # Archive module
-ln -s $PWD/src/dk/netarkivet/archive $M2_BUILD_HOME/netarchivesuite-archive/src/main/java/dk/netarkivet/
-ln -s $PWD/tests/dk/netarkivet/archive $M2_BUILD_HOME/netarchivesuite-archive/src/test/java/dk/netarkivet/
+rm $M2_BUILD_HOME/netarchivesuite-archive/src/main/java/dk/netarkivet/archive
+ln -s $PWD/src/dk/netarkivet/archive $M2_BUILD_HOME/netarchivesuite-archive/src/main/java/dk/netarkivet
+rm $M2_BUILD_HOME/netarchivesuite-archive/src/test/java/dk/netarkivet/archive
+ln -s $PWD/tests/dk/netarkivet/archive $M2_BUILD_HOME/netarchivesuite-archive/src/test/java/dk/netarkivet
 
 # Harvester module
-ln -s $PWD/src/dk/netarkivet/harvester $M2_BUILD_HOME/netarchivesuite-harvester/src/main/java/dk/netarkivet/
-ln -s $PWD/tests/dk/netarkivet/harvester $M2_BUILD_HOME/netarchivesuite-harvester/src/test/java/dk/netarkivet/
+rm $M2_BUILD_HOME/netarchivesuite-harvester/src/main/java/dk/netarkivet/harvester
+ln -s $PWD/src/dk/netarkivet/harvester $M2_BUILD_HOME/netarchivesuite-harvester/src/main/java/dk/netarkivet
+rm $M2_BUILD_HOME/netarchivesuite-harvester/src/test/java/dk/netarkivet/harvester
+ln -s $PWD/tests/dk/netarkivet/harvester $M2_BUILD_HOME/netarchivesuite-harvester/src/test/java/dk/netarkivet
 
 # Monitor module
-ln -s $PWD/src/dk/netarkivet/monitor $M2_BUILD_HOME/netarchivesuite-monitor/src/main/java/dk/netarkivet/
-ln -s $PWD/tests/dk/netarkivet/monitor $M2_BUILD_HOME/netarchivesuite-monitor/src/test/java/dk/netarkivet/
+rm $M2_BUILD_HOME/netarchivesuite-monitor/src/main/java/dk/netarkivet/monitor
+ln -s $PWD/src/dk/netarkivet/monitor $M2_BUILD_HOME/netarchivesuite-monitor/src/main/java/dk/netarkivet
+rm $M2_BUILD_HOME/netarchivesuite-monitor/src/test/java/dk/netarkivet/monitor
+ln -s $PWD/tests/dk/netarkivet/monitor $M2_BUILD_HOME/netarchivesuite-monitor/src/test/java/dk/netarkivet
 
 # Deploy module
-ln -s $PWD/src/dk/netarkivet/deploy $M2_BUILD_HOME/netarchivesuite-deploy/src/main/java/dk/netarkivet/
-ln -s $PWD/tests/dk/netarkivet/deploy $M2_BUILD_HOME/netarchivesuite-deploy/src/test/java/dk/netarkivet/
+rm $M2_BUILD_HOME/netarchivesuite-deploy/src/main/java/dk/netarkivet/deploy
+ln -s $PWD/src/dk/netarkivet/deploy $M2_BUILD_HOME/netarchivesuite-deploy/src/main/java/dk/netarkivet
+rm $M2_BUILD_HOME/netarchivesuite-deploy/src/test/java/dk/netarkivet/deploy
+ln -s $PWD/tests/dk/netarkivet/deploy $M2_BUILD_HOME/netarchivesuite-deploy/src/test/java/dk/netarkivet
 
 # Viewerproxy module
-ln -s $PWD/src/dk/netarkivet/viewerproxy $M2_BUILD_HOME/netarchivesuite-viewerproxy/src/main/java/dk/netarkivet/
-ln -s $PWD/tests/dk/netarkivet/viewerproxy $M2_BUILD_HOME/netarchivesuite-viewerproxy/src/test/java/dk/netarkivet/
+rm $M2_BUILD_HOME/netarchivesuite-viewerproxy/src/main/java/dk/netarkivet/viewerproxy
+ln -s $PWD/src/dk/netarkivet/viewerproxy $M2_BUILD_HOME/netarchivesuite-viewerproxy/src/main/java/dk/netarkivet
+rm $M2_BUILD_HOME/netarchivesuite-viewerproxy/src/test/java/dk/netarkivet/viewerproxy
+ln -s $PWD/tests/dk/netarkivet/viewerproxy $M2_BUILD_HOME/netarchivesuite-viewerproxy/src/test/java/dk/netarkivet
 
 # Wayback module
-ln -s $PWD/src/dk/netarkivet/wayback $M2_BUILD_HOME/netarchivesuite-wayback/src/main/java/dk/netarkivet/
-ln -s $PWD/tests/dk/netarkivet/wayback $M2_BUILD_HOME/netarchivesuite-wayback/src/test/java/dk/netarkivet/
+rm $M2_BUILD_HOME/netarchivesuite-wayback/src/main/java/dk/netarkivet/wayback
+ln -s $PWD/src/dk/netarkivet/wayback $M2_BUILD_HOME/netarchivesuite-wayback/src/main/java/dk/netarkivet
+rm $M2_BUILD_HOME/netarchivesuite-wayback/src/test/java/dk/netarkivet/wayback
+ln -s $PWD/tests/dk/netarkivet/wayback $M2_BUILD_HOME/netarchivesuite-wayback/src/test/java/dk/netarkivet
 
 cd $WORKING_DIR
