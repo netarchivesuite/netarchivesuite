1d76079f8e4c487b603d7e466be0f55441a8d526
svcarlsen <svc@kb.dk>
2011-10-21 18:08:50 +0000

    Fix compile-error.

--- common/common/src/main/java/dk/netarkivet/common/distribute/arcrepository/BitarchiveRecord.java
+++ common/common/src/main/java/dk/netarkivet/common/distribute/arcrepository/BitarchiveRecord.java
@@ -46,7 +46,7 @@ import dk.netarkivet.common.exceptions.IllegalState;
 import dk.netarkivet.common.utils.FileUtils;
 import dk.netarkivet.common.utils.Settings;
 import dk.netarkivet.common.utils.arc.ARCUtils;
-import dk.netarkivet.common.utils.warc.WARCUtils;
+import dk.netarkivet.common.utils.WARCUtils;
 
 /**
  * Class to hold the result of a lookup operation in the bitarchive:
