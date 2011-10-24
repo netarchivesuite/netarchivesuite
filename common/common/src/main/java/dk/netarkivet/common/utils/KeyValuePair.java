3eaeee722576b07d32db3c2529774b21f7df9a94
svcarlsen <svc@kb.dk>
2011-10-24 13:07:45 +0000

    Cosmetic changes.

--- common/common/src/main/java/dk/netarkivet/common/utils/KeyValuePair.java
+++ common/common/src/main/java/dk/netarkivet/common/utils/KeyValuePair.java
@@ -27,12 +27,20 @@ import java.util.Map;
 
 /**
  * A generic Map.Entry class, useful for returning key-value-like results.
- *
+ * @param <K> the Object type used as key
+ * @param <V> the Object type used as value
  */
-
-public class KeyValuePair<K, V> implements Map.Entry<K, V>{
-    final K key;
-    final V value;
+public class KeyValuePair<K, V> implements Map.Entry<K, V> {
+    /** The key in this key-value pair. */
+    private final K key;
+    /** The value in this key-value pair. */
+    private final V value;
+    
+    /**
+     * Constructs a Key-Value pair using the given key and value.
+     * @param k The key object
+     * @param v The value object
+     */
     public KeyValuePair(K k, V v) {
         this.key = k;
         this.value = v;
@@ -57,15 +65,17 @@ public class KeyValuePair<K, V> implements Map.Entry<K, V>{
     }
 
     /**
-     * Replaces the value corresponding to this entry with the specified
-     * value (optional operation).
-     *
-     * @param value new value to be stored in this entry.
+     * Replaces the value corresponding to this entry with the specified value
+     * (optional operation).
+     * 
+     * @param newValue
+     *            new value to be stored in this entry.
      * @return old value corresponding to the entry.
-     * @throws UnsupportedOperationException if the <tt>put</tt> operation
-     *                                       is not supported by the backing map.
+     * @throws UnsupportedOperationException
+     *             if the <tt>put</tt> operation is not supported by the backing
+     *             map.
      */
-    public V setValue(V value) {
+    public V setValue(V newValue) throws UnsupportedOperationException {
         throw new UnsupportedOperationException("Stand-alone entries cannot "
                 + "be changed");
     }
