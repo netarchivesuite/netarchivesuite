dbec38a6f3b5e9ae4346591c10903335dcd15bdb
svcarlsen <svc@kb.dk>
2011-10-24 11:46:06 +0000

    Work related to NAS-1952

--- harvester/harvester/src/main/java/dk/netarkivet/harvester/datamodel/HarvestRunInfo.java
+++ harvester/harvester/src/main/java/dk/netarkivet/harvester/datamodel/HarvestRunInfo.java
@@ -29,9 +29,7 @@ import java.util.Map;
 
 /**
  * Information on a single run of a harvest.
- *
  */
-
 public class HarvestRunInfo {
     /** Which harvest def ID this is a run of. */
     private final long harvestID;
@@ -50,37 +48,43 @@ public class HarvestRunInfo {
     /** Status of the jobs used to run this harvest. */
     private final Map<JobStatus, Integer> jobCounts
             = new HashMap<JobStatus, Integer>();
-
+    /**
+     * Constructor used to read harvest run information from database. 
+     * @param harvestID the ID of the harvest job
+     * @param harvestName the name of the harvest job
+     * @param runNr the run number of this harvest job
+     */
     HarvestRunInfo(long harvestID, String harvestName, int runNr) {
         this.harvestID = harvestID;
         this.harvestName = harvestName;
         this.runNr = runNr;
     }
-
+    /** @return the harvest id of this job */
     public long getHarvestID() {
         return harvestID;
     }
-
+    /** @return the harvest name of this job*/
     public String getHarvestName() {
         return harvestName;
     }
-
+    /** @return the harvest run number of this job */
     public int getRunNr() {
         return runNr;
     }
-
+    /** @return the date when this job started */
     public Date getStartDate() {
         return startDate;
     }
-
+    /** @return the date when this job ended */
     public Date getEndDate() {
         return endDate;
     }
-
+    /** @return bytes harvested by the job */
     public long getBytesHarvested() {
         return bytesHarvested;
     }
-
+    
+    /** @return documents harvested by the job */
     public long getDocsHarvested() {
         return docsHarvested;
     }
@@ -107,23 +111,42 @@ public class HarvestRunInfo {
             return 0;
         }
     }
-
+    /**
+     * Set the start Date for this harvest job.
+     * @param startDate the start date
+     */
     public void setStartDate(Date startDate) {
         this.startDate = startDate;
     }
-
+    /**
+     * Set the end Date for this harvest job.
+     * @param endDate The end date
+     */
     public void setEndDate(Date endDate) {
         this.endDate = endDate;
     }
-
+    
+    /**
+     * Set the number of bytes harvested by this job. 
+     * @param bytesHarvested number of bytes harvested
+     */
     public void setBytesHarvested(long bytesHarvested) {
         this.bytesHarvested = bytesHarvested;
     }
 
+    /**
+     * Set the number of documents harvested by this job. 
+     * @param docsHarvested number of documents harvested
+     */
     public void setDocsHarvested(long docsHarvested) {
         this.docsHarvested = docsHarvested;
     }
-
+    
+    /**
+     * Update the count for a specific jobstatus.
+     * @param status a certain JobStatus
+     * @param count the new count for this JobStatus.
+     */
     public void setStatusCount(JobStatus status, int count) {
         jobCounts.put(status, count);
     }
