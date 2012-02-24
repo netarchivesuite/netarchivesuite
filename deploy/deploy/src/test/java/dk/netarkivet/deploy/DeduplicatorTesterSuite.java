efa77f31836b649a60773f59703c791ec85ec022
mikisseth <mss@statsbiblioteket.dk>
2012-02-24 13:51:08 +0000

    NAS-8: Switch to Maven as build tool: Merging.

---
+++ deploy/deploy/src/test/java/dk/netarkivet/deploy/DeduplicatorTesterSuite.java
@@ -0,0 +1,59 @@
+/* File:    $Id$
+ * Version: $Revision$
+ * Date:    $Date$
+ * Author:  $Author$
+ *
+ * The Netarchive Suite - Software to harvest and preserve websites
+ * Copyright 2004-2012 The Royal Danish Library, the Danish State and
+ * University Library, the National Library of France and the Austrian
+ * National Library.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package dk.netarkivet.deploy;
+
+import dk.netarkivet.archive.arcrepository.ARCLookupTester;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+import dk.netarkivet.archive.indexserver.CDXOriginCrawlLogIteratorTester;
+
+/**
+ * This class runs all tests of Deduplicator functionality.  Must be run as part
+ * of deduplicator upgrades.
+ *
+ */
+
+public class DeduplicatorTesterSuite {
+    public static void addToSuite(TestSuite suite) {
+        suite.addTestSuite(CDXOriginCrawlLogIteratorTester.class);
+        suite.addTestSuite(ARCLookupTester.class);
+    }
+
+    public static Test suite() {
+        TestSuite suite;
+        suite = new TestSuite("DeduplicatorTesterSuite");
+
+        addToSuite(suite);
+
+        return suite;
+    }
+
+    public static void main(String args[]) {
+        String args2[] = {"-noloading", "dk.netarkivet.DeduplicatorTesterSuite"};
+        TestRunner.main(args2);
+    }}
