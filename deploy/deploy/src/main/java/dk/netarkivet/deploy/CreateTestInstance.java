a2d7446d035374bea9ea40e01e04c9a7c3e0506c
csrster <csr@statsbiblioteket.dk>
2012-03-08 13:06:56 +0000

    Removed test instance system for db ports.

--- deploy/deploy/src/main/java/dk/netarkivet/deploy/CreateTestInstance.java
+++ deploy/deploy/src/main/java/dk/netarkivet/deploy/CreateTestInstance.java
@@ -141,10 +141,10 @@ public class CreateTestInstance {
                         Constants.COMPLETE_HARVEST_HERITRIX_GUI_PORT_PATH),
                 new OffsetSystem(Constants.TEST_OFFSET_HERITRIX_JMX_PORT, 
                         Constants.COMPLETE_HARVEST_HERITRIX_JMX_PORT),
-                new OffsetSystem(Constants.TEST_OFFSET_ARCHIVE_DB_URL_PORT,
-                        Constants.COMPLETE_ARCHIVE_DATABASE_PORT),
-                new OffsetSystem(Constants.TEST_OFFSET_HARVEST_DB_URL_PORT,
-                        Constants.COMPLETE_HARVEST_DATABASE_PORT)
+               // new OffsetSystem(Constants.TEST_OFFSET_ARCHIVE_DB_URL_PORT,
+               //         Constants.COMPLETE_ARCHIVE_DATABASE_PORT),
+               // new OffsetSystem(Constants.TEST_OFFSET_HARVEST_DB_URL_PORT,
+               //         Constants.COMPLETE_HARVEST_DATABASE_PORT)
                 };
 
         // apply the arguments
