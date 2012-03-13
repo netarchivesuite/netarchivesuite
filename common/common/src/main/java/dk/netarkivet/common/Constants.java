f11b3ffb89b76d9b006a6c37be2ee938ca13b8f6
svcarlsen <svc@kb.dk>
2012-03-13 16:44:31 +0000

    Make codefreeze for 3.18.2 release containing the fix to NAS-2045 (rollback of parallel indexing)

--- common/common/src/main/java/dk/netarkivet/common/Constants.java
+++ common/common/src/main/java/dk/netarkivet/common/Constants.java
@@ -82,9 +82,9 @@ public final class Constants {
     /** Minor version number. */
     public static final int MINORVERSION = 18;
     /** Patch version number. */
-    public static final int PATCHVERSION = 1;
+    public static final int PATCHVERSION = 2;
     /** Current status of code. */
-    private static final CodeStatus BUILDSTATUS = CodeStatus.UNSTABLE;
+    private static final CodeStatus BUILDSTATUS = CodeStatus.CODEFREEZE;
 
     /** Current version of Heritrix used by netarkivet-code. */
     private static final String HERITRIX_VERSION = "1.14.4";
