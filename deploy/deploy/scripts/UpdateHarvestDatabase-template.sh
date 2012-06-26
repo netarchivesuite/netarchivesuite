afc81747ac6f83c45ca7eb159f668b821cf5f695
svcarlsen <svc@kb.dk>
2012-06-26 16:24:11 +0000

    Template for script to update Harvest database. This is currently only done automatically when using Derby as DBMS

---
+++ deploy/deploy/scripts/UpdateHarvestDatabase-template.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export INSTALLDIR=/home/test/QUICKSTART
+cd $INSTALLDIR
+export CLASSPATH=$INSTALLDIR/lib/dk.netarkivet.harvester.jar:$INSTALLDIR/lib/dk.netarkivet.archive.jar:$INSTALLDIR/lib/dk.netarkivet.viewerproxy.jar:$INSTALLDIR/lib/dk.netarkivet.monitor.jar:$INSTALLDIR/lib/dk.netarkivet.wayback.jar
+java -Ddk.netarkivet.settings.file=$INSTALLDIR/conf/settings_GUIApplication.dk.netarkivet.harvester.tools.HarvestdatabaseUpdateApplication
