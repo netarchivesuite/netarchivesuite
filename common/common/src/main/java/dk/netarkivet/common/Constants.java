cbe1d077ea8af348e8a5bd94faaf82dd95a9283e
svcarlsen <svc@kb.dk>
2013-01-25 18:57:29 +0000

    Moving from codefreeze to code free!

--- common/common/src/main/java/dk/netarkivet/common/Constants.java
+++ common/common/src/main/java/dk/netarkivet/common/Constants.java
@@ -84,11 +84,11 @@ public final class Constants {
     /** Major version number. */
     public static final int MAJORVERSION = 4;
     /** Minor version number. */
-    public static final int MINORVERSION = 0;
+    public static final int MINORVERSION = 1;
     /** Patch version number. */
     public static final int PATCHVERSION = 0;
     /** Current status of code. */
-    private static final CodeStatus BUILDSTATUS = CodeStatus.RELEASE;
+    private static final CodeStatus BUILDSTATUS = CodeStatus.UNSTABLE;
 
     /** Current version of Heritrix used by netarkivet-code. */
     private static final String HERITRIX_VERSION = "1.14.4";
