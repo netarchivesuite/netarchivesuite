49fac0e223ccecff1a53b850a1a057d6171dacb2
svcarlsen <svc@kb.dk>
2013-04-12 15:03:31 +0000

    Add additional-stuff to cleanup

--- common/common/src/test/java/dk/netarkivet/common/CleanupTester.java
+++ common/common/src/test/java/dk/netarkivet/common/CleanupTester.java
@@ -31,9 +31,10 @@ import java.io.FileFilter;
 import junit.framework.TestCase;
 import dk.netarkivet.common.utils.FileUtils;
 
-
 public class CleanupTester extends TestCase {
-	
+
+    private String[] dirsToClean = new String[]{"derbyDB/wayback_indexer_db", "oldjobs"};
+    
 	private File tmpdir;
 	
 	@Override
@@ -56,6 +57,16 @@ public class CleanupTester extends TestCase {
     	for (File f: files) {
     		FileUtils.removeRecursively(f);
     	}
+    	File tmp = new File("tmp");
+    	File tmp1 = new File("tmp1");
+    	FileUtils.remove(tmp);
+    	FileUtils.remove(tmp1);
+    	for (String fileToDelete : dirsToClean) {
+    	    File f = new File(fileToDelete);
+    	    System.out.println("Ready to delete file " + f.getAbsolutePath());
+    	    FileUtils.removeRecursively(f);
+    	}
+    	
     } 
     
     class SvnFileFilter implements FileFilter {	
