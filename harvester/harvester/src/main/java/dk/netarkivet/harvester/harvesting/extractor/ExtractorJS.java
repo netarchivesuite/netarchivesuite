2920714a77eae11eb6a335485f99dfa81c09d327
svcarlsen <svc@kb.dk>
2013-08-01 16:53:38 +0000

    Added NetarchiveSuite header in front of the existing IA header.

--- harvester/harvester/src/main/java/dk/netarkivet/harvester/harvesting/extractor/ExtractorJS.java
+++ harvester/harvester/src/main/java/dk/netarkivet/harvester/harvesting/extractor/ExtractorJS.java
@@ -1,3 +1,30 @@
+/* File:        $Id$
+ * Revision:    $Revision$
+ * Author:      $Author$
+ * Date:        $Date$
+ *
+ * Copyright 2004-2012 The Royal Danish Library, the Danish State and
+ * University Library, the National Library of France and the Austrian
+ * National Library.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * 
+ * Copied from heritrix-1.15.5/src/java/org/archive/crawler/extractor/ExtractorJS.java
+ * 
+ */
+
 /* Copyright (C) 2003 Internet Archive.
  *
  * This file is part of the Heritrix web crawler (crawler.archive.org).
@@ -18,10 +45,6 @@
  *
  * Created on Nov 17, 2003
  *
- * To change the template for this generated file go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
- * 
- * Copied from Heritrix 1.15.5 src distribution
  */
 package dk.netarkivet.harvester.harvesting.extractor;
 
