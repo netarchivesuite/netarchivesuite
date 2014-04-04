c20ccacea8bda5ab4312d0d34e821e161b5b9a2e
ravn <tra@statsbiblioteket.dk>
2014-04-04 12:30:46 +0000

    Updated version to 4.4-UNSTABLE

--- common/common/src/main/java/dk/netarkivet/common/Constants.java
+++ common/common/src/main/java/dk/netarkivet/common/Constants.java
@@ -84,7 +84,7 @@ public final class Constants {
     /** Major version number. */
     public static final int MAJORVERSION = 4;
     /** Minor version number. */
-    public static final int MINORVERSION = 3;
+    public static final int MINORVERSION = 4;
     /** Patch version number. */
     public static final int PATCHVERSION = 0;
     /** Current status of code. */
