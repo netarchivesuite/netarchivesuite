539e7be7856c0cb35c6de9cf803f8cf5545d3faa
Thorbjoern Ravn Andersen <tra@statsbiblioteket.dk>
2014-06-02 13:45:05 +0200

    common: tests converted to junit4.

--- common/common/src/test/java/dk/netarkivet/common/CommonSettingsTester.java
+++ common/common/src/test/java/dk/netarkivet/common/CommonSettingsTester.java
@@ -1,46 +1,30 @@
-/* File:    $Id$
-* Revision: $Revision$
-* Author:   $Author$
-*
-* The Netarchive Suite - Software to harvest and preserve websites
-* Copyright 2004-2012 The Royal Danish Library, the Danish State and
- * University Library, the National Library of France and the Austrian
- * National Library.
-*
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2.1 of the License, or (at your option) any later version.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this library; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*/
 package dk.netarkivet.common;
 
+import static java.lang.reflect.Modifier.isFinal;
+import static java.lang.reflect.Modifier.isPublic;
+import static java.lang.reflect.Modifier.isStatic;
+import static org.junit.Assert.assertFalse;
+
 import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
-/** Unittestersuite for the CommonSettings class. */
-public class CommonSettingsTester extends TestCase {
+/**
+ * Test that all the public static fields for the CommonSettings class are
+ * <i>not</i> final.
+ * 
+ */
+public class CommonSettingsTester {
 
-    @SuppressWarnings("rawtypes")
-	public void testNoFinalSettingsConstants() {
-        Class c = CommonSettings.class;
+    @Test
+    public void testNoFinalSettingsConstants() {
+        Class<CommonSettings> c = CommonSettings.class;
         Field[] fields = c.getDeclaredFields();
-        for (Field f: fields) {
-            // Check that all static public fields are not final
+        for (Field f : fields) {
             int modifiers = f.getModifiers();
-            if (Modifier.isPublic(modifiers) && Modifier.isStatic(modifiers)) {
-                assertFalse("public static fields must not be final, but this was violated by field " + f.getName(),            
-                         Modifier.isFinal(modifiers));
+            if (isPublic(modifiers) && isStatic(modifiers)) {
+                assertFalse("CommonSettings: field final: " + f.getName(),
+                        isFinal(modifiers));
             }
         }
     }
