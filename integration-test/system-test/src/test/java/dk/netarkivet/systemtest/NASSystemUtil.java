736042c6fbd502002c11542c923e0eee26dac8ef
Mikis Seth Sorensen <mss@statsbiblioteket.dk>
2014-09-02 13:05:04 +0200

    Updated SystemOverviewTest to comply with the changed application names

--- integration-test/system-test/src/test/java/dk/netarkivet/systemtest/NASSystemUtil.java
+++ integration-test/system-test/src/test/java/dk/netarkivet/systemtest/NASSystemUtil.java
@@ -30,22 +30,22 @@ public class NASSystemUtil {
      * Defines the standard application setup in the DK test system.
      */
     public static Application[] getApplications() {
-        return new Application[] {new Application("KB-TEST-BAR-015", "BitarchiveServer", null, null, "KBN"),
-                new Application("KB-TEST-BAR-014", "BitarchiveServer", "BitApp_1", null, "KBN"),
-                new Application("KB-TEST-BAR-014", "BitarchiveServer", "BitApp_2", null, "KBN"),
-                new Application("KB-TEST-BAR-014", "BitarchiveServer", "BitApp_3", null, "KBN"),
-                new Application("kb-test-acs-001", "ChecksumFileServer", null, null, "CSN"),
-                new Application("kb-test-acs-001", "IndexServer", null, null, "KBN"),
-                new Application("kb-test-acs-001", "ViewerProxy", null, null, "KBN"),
-                new Application("kb-test-adm-001", "ArcRepository", null, null, "KBN"),
-                new Application("kb-test-adm-001", "BitarchiveMonitorServer", "KBBM", null, "KBN"),
-                new Application("kb-test-adm-001", "BitarchiveMonitorServer", "SBBM", null, "SBN"),
+        return new Application[] {new Application("KB-TEST-BAR-015", "BitarchiveApplication", null, null, "KBN"),
+                new Application("KB-TEST-BAR-014", "BitarchiveApplication", "BitApp_1", null, "KBN"),
+                new Application("KB-TEST-BAR-014", "BitarchiveApplication", "BitApp_2", null, "KBN"),
+                new Application("KB-TEST-BAR-014", "BitarchiveApplication", "BitApp_3", null, "KBN"),
+                new Application("kb-test-acs-001", "ChecksumFileApplication", null, null, "CSN"),
+                new Application("kb-test-acs-001", "IndexServerApplication", null, null, "KBN"),
+                new Application("kb-test-acs-001", "ViewerProxyApplication", null, null, "KBN"),
+                new Application("kb-test-adm-001", "ArcRepositoryApplication", null, null, "KBN"),
+                new Application("kb-test-adm-001", "BitarchiveMonitorApplication", "KBBM", null, "KBN"),
+                new Application("kb-test-adm-001", "BitarchiveMonitorApplication", "SBBM", null, "SBN"),
                 new Application("kb-test-adm-001", "HarvestJobManagerApplication", null, null, "KBN"),
-                new Application("kb-test-adm-001", "GUIWebServer", null, null, "KBN"),
-                new Application("kb-test-har-003", "HarvestControllerServer", "kblow001", "LOWPRIORITY", "KBN"),
-                new Application("kb-test-har-004", "HarvestControllerServer", "kbhigh", "HIGHPRIORITY", "KBN"),
-                new Application("kb-test-har-004", "HarvestControllerServer", "kblow002", "LOWPRIORITY", "KBN"),
-                new Application("sb-test-bar-001", "BitarchiveServer", null, null, "SBN"),
-                new Application("sb-test-har-001", "HarvestControllerServer", "sbhigh", "HIGHPRIORITY", "SBN")};
+                new Application("kb-test-adm-001", "GUIApplication", null, null, "KBN"),
+                new Application("kb-test-har-003", "HarvestControllerApplication", "kblow001", "LOWPRIORITY", "KBN"),
+                new Application("kb-test-har-004", "HarvestControllerApplication", "kbhigh", "HIGHPRIORITY", "KBN"),
+                new Application("kb-test-har-004", "HarvestControllerApplication", "kblow002", "LOWPRIORITY", "KBN"),
+                new Application("sb-test-bar-001", "BitarchiveApplication", null, null, "SBN"),
+                new Application("sb-test-har-001", "HarvestControllerApplication", "sbhigh", "HIGHPRIORITY", "SBN")};
     }
 }
