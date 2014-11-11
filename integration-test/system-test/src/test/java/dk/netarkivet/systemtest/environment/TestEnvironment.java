bf76447d1ad1d0b1542e11b48f691aa91bc116dd
Colin Rosenthal <csr@statsbiblioteket.dk>
2014-11-11 09:21:54 +0100

    Made these non-final. Someone might want to create a subclass with overrides.

--- integration-test/system-test/src/test/java/dk/netarkivet/systemtest/environment/TestEnvironment.java
+++ integration-test/system-test/src/test/java/dk/netarkivet/systemtest/environment/TestEnvironment.java
@@ -23,8 +23,8 @@
 package dk.netarkivet.systemtest.environment;
 
 public interface TestEnvironment {
-    public static final String DEPLOYMENT_SERVER = "kb-prod-udv-001.kb.dk";
-    public static final String JOB_ADMIN_SERVER = "kb-test-adm-001.kb.dk";
-    public static final String ARCHIVE_ADMIN_SERVER = "kb-test-adm-001.kb.dk";
-    public static final String CHECKSUM_SERVER = "kb-test-acs-001.kb.dk";
+    public static String DEPLOYMENT_SERVER = "kb-prod-udv-001.kb.dk";
+    public static String JOB_ADMIN_SERVER = "kb-test-adm-001.kb.dk";
+    public static String ARCHIVE_ADMIN_SERVER = "kb-test-adm-001.kb.dk";
+    public static String CHECKSUM_SERVER = "kb-test-acs-001.kb.dk";
 }
