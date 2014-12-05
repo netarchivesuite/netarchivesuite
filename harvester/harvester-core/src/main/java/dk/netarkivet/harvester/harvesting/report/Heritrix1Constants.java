ad98d14efbcd673b4d2d81a64c5b8140e0d8cf79
Nicholas Clarke <nicl@kb.dk>
2014-12-05 19:24:12 +0100

    Trying to un-depend harvester-core from heritrix 1.

--- harvester/harvester-core/src/main/java/dk/netarkivet/harvester/harvesting/report/Heritrix1Constants.java
+++ harvester/harvester-core/src/main/java/dk/netarkivet/harvester/harvesting/report/Heritrix1Constants.java
@@ -22,33 +22,39 @@
  */
 
 package dk.netarkivet.harvester.harvesting.report;
-
+/*
 import org.archive.crawler.datamodel.CrawlURI;
 import org.archive.crawler.deciderules.DecideRuleSequence;
 import org.archive.crawler.deciderules.DecidingScope;
 import org.archive.crawler.deciderules.MatchesListRegExpDecideRule;
 import org.archive.crawler.framework.CrawlController;
-
+*/
 public class Heritrix1Constants {
 
     /** Prefix associated with annotations made by this processor. */
     public static final String CONTENT_SIZE_ANNOTATION_PREFIX = "content-size:";
 
     // import org.archive.crawler.datamodel.CrawlURI;
-    public static final int CRAWLURI_S_BLOCKED_BY_QUOTA = CrawlURI.S_BLOCKED_BY_QUOTA;
+    //public static final int CRAWLURI_S_BLOCKED_BY_QUOTA = CrawlURI.S_BLOCKED_BY_QUOTA;
+    public static final int CRAWLURI_S_BLOCKED_BY_QUOTA = -5003;
 
     // import org.archive.crawler.deciderules.DecideRuleSequence;
-    public static final String DECIDERULESEQUENCE_CLASSNAME = DecideRuleSequence.class.getName();
+    //public static final String DECIDERULESEQUENCE_CLASSNAME = DecideRuleSequence.class.getName();
+    public static final String DECIDERULESEQUENCE_CLASSNAME = "org.archive.crawler.deciderules.DecideRuleSequence";
 
     // import org.archive.crawler.deciderules.DecidingScope;
-    public static final String DECIDINGSCOPE_CLASSNAME = DecidingScope.class.getName();
+    //public static final String DECIDINGSCOPE_CLASSNAME = DecidingScope.class.getName();
+    public static final String DECIDINGSCOPE_CLASSNAME = "org.archive.crawler.deciderules.DecidingScope";
 
     // import org.archive.crawler.deciderules.MatchesListRegExpDecideRule;
-    public static final String MATCHESLISTREGEXPDECIDERULE_CLASSNAME = MatchesListRegExpDecideRule.class.getName();
+    //public static final String MATCHESLISTREGEXPDECIDERULE_CLASSNAME = MatchesListRegExpDecideRule.class.getName();
+    public static final String MATCHESLISTREGEXPDECIDERULE_CLASSNAME = "org.archive.crawler.deciderules.MatchesListRegExpDecideRule";
 
     // import org.archive.crawler.framework.CrawlController;
-    public static final Object CRAWLCONTROLLER_FINISHED = CrawlController.FINISHED;
+    //public static final Object CRAWLCONTROLLER_FINISHED = CrawlController.FINISHED;
+    public static final Object CRAWLCONTROLLER_FINISHED = "FINISHED".intern();
 
+    /*
     public static void main(String[] args) {
         System.out.println(CRAWLURI_S_BLOCKED_BY_QUOTA);
         System.out.println(DECIDERULESEQUENCE_CLASSNAME);
@@ -56,5 +62,6 @@ public class Heritrix1Constants {
         System.out.println(MATCHESLISTREGEXPDECIDERULE_CLASSNAME);
         System.out.println(CRAWLCONTROLLER_FINISHED);
     }
+    */
 
 }
