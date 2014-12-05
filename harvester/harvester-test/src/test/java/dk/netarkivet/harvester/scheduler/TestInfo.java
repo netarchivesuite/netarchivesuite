582d073ae9f2ff15939790db532089690a4fbfb8
Nicholas Clarke <nicl@kb.dk>
2014-12-05 21:37:28 +0100

    harvester-test used TestInfo from harvest-scheduler.

---
+++ harvester/harvester-test/src/test/java/dk/netarkivet/harvester/scheduler/TestInfo.java
@@ -0,0 +1,48 @@
+/*
+ * #%L
+ * Netarchivesuite - harvester - test
+ * %%
+ * Copyright (C) 2005 - 2014 The Royal Danish Library, the Danish State and University Library,
+ *             the National Library of France and the Austrian National Library.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+package dk.netarkivet.harvester.scheduler;
+
+import java.io.File;
+
+/**
+ * Contains test information about all scheduler test data.
+ */
+public class TestInfo {
+
+    /**
+     * The properties-file containing properties for logging in unit-tests.
+     */
+    static final File TESTLOGPROP = new File("tests/dk/netarkivet/testlog.prop");
+
+    static final File BASEDIR = new File("tests/dk/netarkivet/harvester/scheduler/data");
+    static final File ORIGINALS_DIR = new File(BASEDIR, "originals");
+    static final File WORKING_DIR = new File(BASEDIR, "working");
+
+    static File ORDER_FILE = new File(TestInfo.WORKING_DIR, "order.xml");
+
+    static final File orderTemplatesOriginalsDir = new File("tests/dk/netarkivet/"
+            + "/harvester/data/originals/order_templates/");
+
+    public TestInfo() {
+    }
+}
