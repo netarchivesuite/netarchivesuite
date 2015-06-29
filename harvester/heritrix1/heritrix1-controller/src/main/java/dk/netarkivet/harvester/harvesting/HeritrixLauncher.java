2efb14f09702245e9ccf2f71bd4376306725175c
svcarlsen <svc@kb.dk>
2015-06-29 15:11:25 +0200

    Fixed NAS-2428. The H1 HeritrixLauncher now checks that the job contains the correct kind of template

--- harvester/heritrix1/heritrix1-controller/src/main/java/dk/netarkivet/harvester/harvesting/HeritrixLauncher.java
+++ harvester/heritrix1/heritrix1-controller/src/main/java/dk/netarkivet/harvester/harvesting/HeritrixLauncher.java
@@ -26,6 +26,8 @@ import dk.netarkivet.common.exceptions.ArgumentNotValid;
 import dk.netarkivet.common.exceptions.IOFailure;
 import dk.netarkivet.common.utils.Settings;
 import dk.netarkivet.harvester.HarvesterSettings;
+import dk.netarkivet.harvester.datamodel.H1HeritrixTemplate;
+import dk.netarkivet.harvester.datamodel.H3HeritrixTemplate;
 import dk.netarkivet.harvester.datamodel.HeritrixTemplate;
 
 /**
@@ -97,7 +99,53 @@ public abstract class HeritrixLauncher {
     }
 
     public void setupOrderfile(HeritrixFiles files) {
-        HeritrixTemplate.makeTemplateReadyForHeritrix1(files);
+        makeTemplateReadyForHeritrix1(files);
     }
+    
+    /**
+     * 
+     * Updates the diskpath value, archivefile_prefix, seedsfile, and deduplication -information.
+     * @param files Files associated with a Heritrix1 crawl-job.
+     * @throws IOFailure
+     */
+    /**
+     * This method prepares the orderfile used by the Heritrix crawler. </p> 1. alters the orderfile in the
+     * following-way: (overriding whatever is in the orderfile)</br>
+     * <ol>
+     * <li>sets the disk-path to the outputdir specified in HeritrixFiles.</li>
+     * <li>sets the seedsfile to the seedsfile specified in HeritrixFiles.</li>
+     * <li>sets the prefix of the arcfiles to unique prefix defined in HeritrixFiles</li>
+     * <li>checks that the arcs-file dir is 'arcs' - to ensure that we know where the arc-files are when crawl finishes</li>
+     * <p>
+     * <li>if deduplication is enabled, sets the node pointing to index directory for deduplication (see step 3)</li>
+     * </ol>
+     * 2. saves the orderfile back to disk</p>
+     * <p>
+     * 3. if deduplication is enabled in the order.xml, it writes the absolute path of the lucene index used by the
+     * deduplication processor.
+     *
+     * @throws IOFailure - When the orderfile could not be saved to disk 
+     *                     When a specific element cannot be found in the document. 
+     */
+    public static void makeTemplateReadyForHeritrix1(HeritrixFiles files) throws IOFailure {
+    	HeritrixTemplate templ = HeritrixTemplate.read(files.getOrderXmlFile());
+    	// Verify that the template in the job is a Heritrix3Template
+    	if (templ instanceof H1HeritrixTemplate) {
+    		templ.setDiskPath(files.getCrawlDir().getAbsolutePath());
+    		templ.setArchiveFilePrefix(files.getArchiveFilePrefix());
+    		templ.setSeedsFilePath(files.getSeedsTxtFile().getAbsolutePath());
+    		if (templ.IsDeduplicationEnabled()) {
+    			templ.setDeduplicationIndexLocation(files.getIndexDir().getAbsolutePath());
+    		}
+    		files.writeOrderXml(templ);
+    	} else {
+    		
+    	}
+    }
+
+    
+    
+    
+    
 
 }
