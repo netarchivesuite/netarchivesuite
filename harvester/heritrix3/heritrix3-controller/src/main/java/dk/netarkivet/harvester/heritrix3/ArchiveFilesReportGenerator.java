aa3fe7215e7f128d73714362832ec449eb13cd8c
svcarlsen <svc@kb.dk>
2015-11-19 13:16:25 +0100

    NAS-2470 - fixed some indentation

--- harvester/heritrix3/heritrix3-controller/src/main/java/dk/netarkivet/harvester/heritrix3/ArchiveFilesReportGenerator.java
+++ harvester/heritrix3/heritrix3-controller/src/main/java/dk/netarkivet/harvester/heritrix3/ArchiveFilesReportGenerator.java
@@ -10,13 +10,13 @@ import dk.netarkivet.common.exceptions.IOFailure;
 import dk.netarkivet.common.utils.Settings;
 
 
-
 /**This class generate a report that lists ARC/WARC files (depending on the configured archive format) along with the
 * opening date, closing date (if file was properly closed), and size in bytes.
 * <p>
 * Here is a sample of such a file:
 * <p>
-* [ARCHIVEFILE] [Closed] [Size] 5-1-20100720161253-00000-bnf_test.arc.gz  "2010-07-20 16:14:31.792" 162928
+* [ARCHIVEFILE] [Closed] [Size]<p> 
+* 5-1-20100720161253-00000-bnf_test.arc.gz  "2010-07-20 16:14:31.792" 162928
 * <p>
 * The file is named "archivefiles-report.txt"
 */
@@ -47,37 +47,37 @@ class ArchiveFilesReportGenerator {
 		this.ingestablefiles = ingestableFiles;
 	}   
 
-	    /**
-	     * Parses heritrix.out and generates the ARC/WARC files report.
-	     *
-	     * @return the generated report file.
-	     */
-	    protected File generateReport() {
-	        
-	        File reportFile = new File(ingestablefiles.getCrawlDir(), REPORT_FILE_NAME);
-	        
-	        try {
-	            boolean created = reportFile.createNewFile();
-	            if (!created) {
-	                throw new IOException("Unable to create '" + reportFile.getAbsolutePath() + "'.");
-	            }
-	            PrintWriter out = new PrintWriter(reportFile);
+	/**
+	 * Parses heritrix.out and generates the ARC/WARC files report.
+	 *
+	 * @return the generated report file.
+	 */
+	protected File generateReport() {
 
-	            out.println(REPORT_FILE_HEADER);
+		File reportFile = new File(ingestablefiles.getCrawlDir(), REPORT_FILE_NAME);
 
-	            for (File arcfile : ingestablefiles.getArcFiles()) {
-	            	out.println(arcfile.getName() + " " + ISO_8601_DATE_FORMAT.format(new Date(arcfile.lastModified())) + " " + arcfile.length());
-	            }
-	            for (File warcfile : ingestablefiles.getWarcFiles()) {
-	            	out.println(warcfile.getName() + " " + ISO_8601_DATE_FORMAT.format(new Date(warcfile.lastModified())) + " " + warcfile.length());
-	            }
-	          
-	            out.close();
-	        } catch (IOException e) {
-	            throw new IOFailure("Failed to create " + reportFile.getName(), e);
-	        }
+		try {
+			boolean created = reportFile.createNewFile();
+			if (!created) {
+				throw new IOException("Unable to create '" + reportFile.getAbsolutePath() + "'.");
+			}
+			PrintWriter out = new PrintWriter(reportFile);
 
-	        return reportFile;
-	    }
+			out.println(REPORT_FILE_HEADER);
+
+			for (File arcfile : ingestablefiles.getArcFiles()) {
+				out.println(arcfile.getName() + " " + ISO_8601_DATE_FORMAT.format(new Date(arcfile.lastModified())) + " " + arcfile.length());
+			}
+			for (File warcfile : ingestablefiles.getWarcFiles()) {
+				out.println(warcfile.getName() + " " + ISO_8601_DATE_FORMAT.format(new Date(warcfile.lastModified())) + " " + warcfile.length());
+			}
+
+			out.close();
+		} catch (IOException e) {
+			throw new IOFailure("Failed to create " + reportFile.getName(), e);
+		}
+
+		return reportFile;
+	}
 
 }
