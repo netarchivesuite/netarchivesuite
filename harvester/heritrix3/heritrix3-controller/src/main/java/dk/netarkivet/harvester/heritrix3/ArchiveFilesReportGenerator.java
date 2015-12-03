529faafc7be7c8f72545e99241c68d57c9968510
svcarlsen <svc@kb.dk>
2015-12-03 12:08:13 +0100

    Some more follow-up after review of NAS-2470

--- harvester/heritrix3/heritrix3-controller/src/main/java/dk/netarkivet/harvester/heritrix3/ArchiveFilesReportGenerator.java
+++ harvester/heritrix3/heritrix3-controller/src/main/java/dk/netarkivet/harvester/heritrix3/ArchiveFilesReportGenerator.java
@@ -22,62 +22,61 @@ import dk.netarkivet.common.utils.Settings;
 */
 class ArchiveFilesReportGenerator {
 
-	private static final SimpleDateFormat ISO_8601_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+    private static final SimpleDateFormat ISO_8601_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
-	/**
-	 * The name of the report file. It will be generated in the crawl directory.
-	 */
-	public static final String REPORT_FILE_NAME = Settings.get(Heritrix3Settings.METADATA_ARCHIVE_FILES_REPORT_NAME);
+    /**
+     * The name of the report file. It will be generated in the crawl directory.
+     */
+    public static final String REPORT_FILE_NAME = Settings.get(Heritrix3Settings.METADATA_ARCHIVE_FILES_REPORT_NAME);
 
-	/**
-	 * The header line of the report file.
-	 */
-	public static final String REPORT_FILE_HEADER = Settings
-			.get(Heritrix3Settings.METADATA_ARCHIVE_FILES_REPORT_HEADER);
+    /**
+     * The header line of the report file.
+     */
+    public static final String REPORT_FILE_HEADER = Settings.get(Heritrix3Settings.METADATA_ARCHIVE_FILES_REPORT_HEADER);
 
-	private IngestableFiles ingestablefiles;
+    private IngestableFiles ingestablefiles;
 
-	/**
-	 * Builds a ARC files report generator, given the Ingestable files object.
-	 *
-	 * @param ingestableFiles files belonging to a Heritrix harvest
-	 */
+    /**
+     * Builds a ARC files report generator, given the Ingestable files object.
+     *
+     * @param ingestableFiles files belonging to a Heritrix harvest
+     */
 
-	public ArchiveFilesReportGenerator(IngestableFiles ingestableFiles) {
-		this.ingestablefiles = ingestableFiles;
-	}   
+    public ArchiveFilesReportGenerator(IngestableFiles ingestableFiles) {
+        this.ingestablefiles = ingestableFiles;
+    }   
 
-	/**
-	 * Parses heritrix.out and generates the ARC/WARC files report.
-	 *
-	 * @return the generated report file.
-	 */
-	protected File generateReport() {
+    /**
+     * Parses heritrix.out and generates the ARC/WARC files report.
+     *
+     * @return the generated report file.
+     */
+    protected File generateReport() {
 
-		File reportFile = new File(ingestablefiles.getCrawlDir(), REPORT_FILE_NAME);
+        File reportFile = new File(ingestablefiles.getCrawlDir(), REPORT_FILE_NAME);
 
-		try {
-			boolean created = reportFile.createNewFile();
-			if (!created) {
-				throw new IOException("Unable to create '" + reportFile.getAbsolutePath() + "'.");
-			}
-			PrintWriter out = new PrintWriter(reportFile);
+        try {
+            boolean created = reportFile.createNewFile();
+            if (!created) {
+                throw new IOException("Unable to create '" + reportFile.getAbsolutePath() + "'.");
+            }
+            PrintWriter out = new PrintWriter(reportFile);
 
-			out.println(REPORT_FILE_HEADER);
+            out.println(REPORT_FILE_HEADER);
 
-			for (File arcfile : ingestablefiles.getArcFiles()) {
-				out.println(arcfile.getName() + " " + ISO_8601_DATE_FORMAT.format(new Date(arcfile.lastModified())) + " " + arcfile.length());
-			}
-			for (File warcfile : ingestablefiles.getWarcFiles()) {
-				out.println(warcfile.getName() + " " + ISO_8601_DATE_FORMAT.format(new Date(warcfile.lastModified())) + " " + warcfile.length());
-			}
+            for (File arcfile : ingestablefiles.getArcFiles()) {
+                out.println(arcfile.getName() + " " + ISO_8601_DATE_FORMAT.format(new Date(arcfile.lastModified())) + " " + arcfile.length());
+            }
+            for (File warcfile : ingestablefiles.getWarcFiles()) {
+                out.println(warcfile.getName() + " " + ISO_8601_DATE_FORMAT.format(new Date(warcfile.lastModified())) + " " + warcfile.length());
+            }
 
-			out.close();
-		} catch (IOException e) {
-			throw new IOFailure("Failed to create " + reportFile.getName(), e);
-		}
+            out.close();
+        } catch (IOException e) {
+            throw new IOFailure("Failed to create " + reportFile.getName(), e);
+        }
 
-		return reportFile;
-	}
+        return reportFile;
+    }
 
 }
