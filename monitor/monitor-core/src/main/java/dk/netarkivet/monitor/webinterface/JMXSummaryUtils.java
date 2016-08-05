2404ae8330acfe916a2a4b031f4155812148e560
Colin Rosenthal <csr@statsbiblioteket.dk>
2016-08-05 11:17:27 +0200

    Added some debug logging.

--- monitor/monitor-core/src/main/java/dk/netarkivet/monitor/webinterface/JMXSummaryUtils.java
+++ monitor/monitor-core/src/main/java/dk/netarkivet/monitor/webinterface/JMXSummaryUtils.java
@@ -36,6 +36,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.PageContext;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import dk.netarkivet.common.exceptions.ArgumentNotValid;
 import dk.netarkivet.common.exceptions.ForwardedToErrorPage;
 import dk.netarkivet.common.utils.DomainUtils;
@@ -51,6 +54,10 @@ import dk.netarkivet.monitor.MonitorSettings;
  * Monitor-JMXsummary.jsp.
  */
 public class JMXSummaryUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(JMXSummaryUtils.class);
+
+
     /** JMX property for remove application button. */
     public static final String JMXRemoveApplication = dk.netarkivet.common.management.Constants.REMOVE_JMX_APPLICATION;
     /** JMX property for the physical location. */
@@ -281,6 +288,7 @@ public class JMXSummaryUtils {
         String query = null;
         try {
             query = createJMXQuery(parameters, request);
+            log.debug("Executing JMX query: " + query);
             return JMXStatusEntry.queryJMX(query);
         } catch (MalformedObjectNameException e) {
             if (query != null) {
