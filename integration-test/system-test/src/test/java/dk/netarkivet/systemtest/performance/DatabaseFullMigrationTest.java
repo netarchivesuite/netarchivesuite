9672c9142f4b27d020c8bb662a0c05a6524aa200
Colin Rosenthal <csr@statsbiblioteket.dk>
2016-08-08 09:20:12 +0200

    Re-enabled snapshot job test in stress test.

--- integration-test/system-test/src/test/java/dk/netarkivet/systemtest/performance/DatabaseFullMigrationTest.java
+++ integration-test/system-test/src/test/java/dk/netarkivet/systemtest/performance/DatabaseFullMigrationTest.java
@@ -59,7 +59,7 @@ public class DatabaseFullMigrationTest extends AbstractStressTest {
         doIngestDomains();
     }
 
-    @Test  ( enabled = false )
+    @Test  ( enabled = true )
     public void testGenerateSnapshot() {
         addDescription("Test generating snapshot jobs with a maximum number of bytes per domain of 100 000. This takes about ten hours to complete. The" +
                 " number of jobs generated is determined roughly by the parameter settings.harvester.scheduler.jobGen.domainConfigSubsetSize which is" +
