95eaaeb7f82e27ba8e2e1aa8965fbd2ed6f9678f
Colin Rosenthal <csr@statsbiblioteket.dk>
2017-01-30 12:09:35 +0100

    Fixed concurrency problem NAS-5098

--- wayback/wayback-indexer/src/main/java/dk/netarkivet/wayback/batch/DeduplicateToCDXAdapter.java
+++ wayback/wayback-indexer/src/main/java/dk/netarkivet/wayback/batch/DeduplicateToCDXAdapter.java
@@ -27,10 +27,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.text.SimpleDateFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.time.FastDateFormat;
 import org.archive.wayback.UrlCanonicalizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,8 +48,8 @@ public class DeduplicateToCDXAdapter implements DeduplicateToCDXAdapterInterface
     /** Define SimpleDateFormat objects for the representation of timestamps in crawl logs and cdx files respectively. */
     private static final String crawlDateFormatString = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
     private static final String cdxDateFormatString = "yyyyMMddHHmmss";
-    private static final SimpleDateFormat crawlDateFormat = new SimpleDateFormat(crawlDateFormatString);
-    private static final SimpleDateFormat cdxDateFormat = new SimpleDateFormat(cdxDateFormatString);
+    private static final FastDateFormat crawlDateFormat = FastDateFormat.getInstance(crawlDateFormatString);
+    private static final FastDateFormat cdxDateFormat = FastDateFormat.getInstance(cdxDateFormatString);
 
     /** Pattern representing the part of a crawl log entry describing a duplicate record. */
     private static final String duplicateRecordPatternString = "duplicate:\"([^,]*),([^,]*)\",(.*)";	//e.g. duplicate:"arcfile,offset"
