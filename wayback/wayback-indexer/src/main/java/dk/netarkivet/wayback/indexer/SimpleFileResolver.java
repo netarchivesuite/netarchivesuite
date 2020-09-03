57b380f2f8c289544404aecbb81f6cef8a084274
csrster <csr@statsbiblioteket.dk>
2020-09-03 09:25:03 +0200

    Added pattern-matching method to file-resolver

--- wayback/wayback-indexer/src/main/java/dk/netarkivet/wayback/indexer/SimpleFileResolver.java
+++ wayback/wayback-indexer/src/main/java/dk/netarkivet/wayback/indexer/SimpleFileResolver.java
@@ -1,6 +1,8 @@
 package dk.netarkivet.wayback.indexer;
 
 import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.util.List;
 
 public class SimpleFileResolver implements FileResolver {
 
@@ -8,6 +10,10 @@ public class SimpleFileResolver implements FileResolver {
 
     public SimpleFileResolver(Path directory) {
        this.directory = directory;
+    }
+
+    @Override public List<Path> getPaths(PathMatcher filepattern) {
+        throw new RuntimeException("not implemented");
     }
 
     @Override public Path getPath(String filename) {
